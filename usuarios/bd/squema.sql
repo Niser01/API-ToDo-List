7740f16c0517acb104a2eeae130686b82c0695a7
Niser01 <64512516+Niser01@users.noreply.github.com>
2025-05-27 21:44:24 -0500

    Update squema.sql
    Creación de nuevas tablas y reorganización de las mismas

--- usuarios/bd/squema.sql
+++ usuarios/bd/squema.sql
@@ -1,5 +1,17 @@
+DROP TABLE IF EXISTS tipo_de_autenticacion;
+DROP TABLE IF EXISTS tipo_de_perfiles;
 DROP TABLE IF EXISTS autenticaciones;
 DROP TABLE IF EXISTS usuarios;
+
+CREATE TABLE IF NOT EXISTS tipo_de_autenticacion (
+    id SERIAL PRIMARY KEY,
+    tipo_de_autenticacion TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tipo_de_perfiles(
+    id SERIAL PRIMARY KEY,
+    tipo_de_perfil TEXT UNIQUE NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS usuarios (
     id SERIAL PRIMARY KEY,
@@ -8,6 +20,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
     perfil_activo BOOLEAN DEFAULT TRUE,
     url_foto_perfil TEXT,
     telefono TEXT,
+    tipo_perfil INTEGER REFERENCES tipo_de_perfiles(id) ON DELETE SET NULL, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -18,7 +31,13 @@ CREATE TABLE IF NOT EXISTS autenticaciones (
     correo TEXT UNIQUE NOT NULL,
     password_hash TEXT, 
     oauth_uid TEXT,
+    tipo_autenticacion INTEGER REFERENCES tipo_de_autenticacion(id) ON DELETE SET NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO tipo_de_perfiles (tipo_de_perfil) 
+VALUES ('Buho'), ('Zorro'), ('Gato'), ('Panda');
+
+INSERT INTO tipo_de_autenticacion (tipo_de_autenticacion) 
+VALUES ('Google'), ('Email');
