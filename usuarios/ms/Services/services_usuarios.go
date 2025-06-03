50066c6b8b21973a7173cdebbeeba9efed45eeaf
Niser01 <64512516+Niser01@users.noreply.github.com>
2025-06-02 21:28:20 -0500

    Services para Tipo de Autentiacion
    Se crean las funciones CRUD básicas para el modelo de Tipo de Autenticación

--- usuarios/ms/Services/services_usuarios.go
+++ usuarios/ms/Services/services_usuarios.go
@@ -5,9 +5,50 @@ import (
 	"usuarios/ms/Model"
 )
 
-func CrearTipoDeAutenticacion(nuevoTipo string) error {
+func CreateTipoDeAutenticacion(nuevoTipo string) error {
 	autenticacion := Model.TipoDeAutenticacion{TipoAutenticacion: nuevoTipo}
 	err := DataBase.DB.Create(&autenticacion).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ReadFullTipoDeAutenticacion() ([]Model.TipoDeAutenticacion, error) {
+	var tipos []Model.TipoDeAutenticacion
+	err := DataBase.DB.Find(&tipos).Error
+	if err != nil {
+		return []Model.TipoDeAutenticacion{}, err
+	}
+	return tipos, err
+}
+
+func ReadTipoDeAutenticacionByID(id int) (Model.TipoDeAutenticacion, error) {
+	var autenticacion Model.TipoDeAutenticacion
+	err := DataBase.DB.First(&autenticacion, id).Error
+	if err != nil {
+		return Model.TipoDeAutenticacion{}, err
+	}
+	return autenticacion, nil
+}
+
+func UpdateTipoDeAutenticacion(id int, tipoModificado string) error {
+	var item_a_actualizar Model.TipoDeAutenticacion
+	err := DataBase.DB.First(&item_a_actualizar, id).Error
+	if err != nil {
+		return err
+	}
+	item_a_actualizar.TipoAutenticacion = tipoModificado
+	err = DataBase.DB.Save(&item_a_actualizar).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteTipoDeAutenticacion(id int) error {
+	eliminacion := Model.TipoDeAutenticacion{ID: id}
+	err := DataBase.DB.Delete(&eliminacion).Error
 	if err != nil {
 		return err
 	}
