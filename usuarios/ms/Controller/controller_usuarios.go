9216e93a2830d7df01947e3f2e1b2edfadc2755b
Niser01 <64512516+Niser01@users.noreply.github.com>
2025-06-02 21:47:46 -0500

    Update controller_usuarios.go
    Creacion del primer controlador para la creacion de tipo de autenticacion

--- usuarios/ms/Controller/controller_usuarios.go
+++ usuarios/ms/Controller/controller_usuarios.go
@@ -1,1 +1,24 @@
 package Controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"usuarios/ms/Services"
+)
+
+func ControllerCreateTipoDeAutenticacion(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		NuevoTipo string `json:"NuevoTipo"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, "JSON invalido", http.StatusBadRequest)
+		return
+	}
+	err = Services.CreateTipoDeAutenticacion(body.NuevoTipo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
