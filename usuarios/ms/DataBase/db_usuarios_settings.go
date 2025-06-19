709f49873fa565c046c00aa9075ccd122270b3dc
Niser01 <64512516+Niser01@users.noreply.github.com>
2025-06-19 10:53:18 -0500

    Update db_usuarios_settings.go

--- usuarios/ms/DataBase/db_usuarios_settings.go
+++ usuarios/ms/DataBase/db_usuarios_settings.go
@@ -22,9 +22,7 @@ func InitDB() error {
 	db_port := os.Getenv("DB_PORT")
 	db_sslmode := os.Getenv("SSL_MODE")
 
-	db_connection := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		db_host, db_user, db_password, db_database, db_port, db_sslmode,
-	)
+	db_connection := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", db_host, db_user, db_password, db_database, db_port, db_sslmode)
 
 	DB, err = gorm.Open(postgres.Open(db_connection), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
